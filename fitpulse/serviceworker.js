6f1cac77c14a2e3f385d158b6623e936f118a374
Aurelia_Yi <133084413+yiyuke@users.noreply.github.com>
2025-03-21 10:48:39 -0400

    Update serviceworker.js

--- fitpulse/serviceworker.js
+++ fitpulse/serviceworker.js
@@ -1,9 +1,8 @@
-
 // ZIM Zapps PWA Service Worker to cache app files
 // Please check to see all files have been listed with local links
 // (Do not worry about icon files) 
 
-var cacheName = 'zim_pwa_FitPulse';
+var cacheName = 'zim_pwa_FitPulse_v1';
 var filesToCache = [
   './',
   'index.html',
@@ -19,6 +18,24 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
+  
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
+});
+
+/* Clear old caches when a new service worker is activated */
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  // Take control of all clients immediately
+  return self.clients.claim();
 });
 
 /* Serve cached content when offline */
